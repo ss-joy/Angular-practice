a46bd0e78ba98f367e9a606c3fab3c8066deef00
ss joy <ssjoy.u.b@gmail.com>
2023-10-26 23:14:32 +0600

    add first form :)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'angular-projec';
+  sei() {
+    console.log('blah');
+  }
+  title = 'angular-projecxxxerfffdfsdfsafasdf';
+  chudi = 'bal ama';
 }
